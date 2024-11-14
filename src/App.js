db3ce222887b0b1b8a6b2bf0a894540062f9a0e2
Your Name <you@example.com>
2024-11-14 16:03:52 -0300

    cambios

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-import './componentes/header.jsx';
+import './componentes/header';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 
@@ -8,7 +8,20 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <img src="/logo512.png" className="App-logo" alt="logo" />
+        
+        <p>
+           <code>Estamos trabajando para usted</code>.
+        </p>
       
+        <a
+          className="App-link"
+          href="https://www.dlight.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Visitá nuestros productos
+        </a>
       </header>
     </div>
   );
