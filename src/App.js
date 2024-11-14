e8f0586c679b6721645b792c9a22a1f69631b84a
Your Name <you@example.com>
2024-11-14 13:32:08 -0300

    estamos trabajando

---
+++ src/App.js
@@ -0,0 +1,27 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <img src="/logo512.png" className="App-logo" alt="logo" />
+        
+        <p>
+           <code>Estamos trabajando para usted</code>.
+        </p>
+      
+        <a
+          className="App-link"
+          href="https://www.dlight.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Visitá nuestros productos
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
