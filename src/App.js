e8f6eb9b598938dee20a7ad3297c47c86cae686e
dift12 <diftbanda@gmail.com>
2024-11-17 15:51:31 -0300

    navbar

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
-
 import './App.css';
-import './componentes/header';
+import Header from './componentes/header';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <img src="/logo512.png" className="App-logo" alt="logo" />
-        
-        <p>
-           <code>Estamos trabajando para usted</code>.
-        </p>
-
-      
-        <a
-          className="App-link"
-          href="https://www.dlight.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Visitá nuestros productos
-        </a>
+        <Header /> {/* Forma correcta de usar el componente */}
       </header>
     </div>
   );
