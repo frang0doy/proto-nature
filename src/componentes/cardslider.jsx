4b2dd85217b13406522a93e8935c562dfbcd404e
Francisco Godoy <sonrigodoy65@gmail.com>
2024-12-01 19:46:48 -0300

    card slider

--- src/componentes/cardslider.jsx
+++ src/componentes/cardslider.jsx
@@ -1,50 +1,50 @@
-import React, { useState } from 'react';
-import '../cardslider.css'; 
+import React from "react";
+import "../cardslider.css";
 
 const teamMembers = [
   {
     name: "Tomás Wentzel",
     title: "Web Designer",
-    imgSrc: "./tomas.jpg", // Ruta correcta
+    imgSrc: "./tomas.jpg",
   },
   {
     name: "Marco Emmanuel Ortiz",
     title: "UI Designer",
-    imgSrc: "./marco.jpg", // Ruta correcta
+    imgSrc: "./marco.jpg",
   },
   {
     name: "Michelle Gauweloose",
     title: "Web Developer",
-    imgSrc: "./michelle.jpg", // Ruta correcta
+    imgSrc: "./michelle.jpg",
   },
   {
     name: "Roberto Fazzito",
     title: "Mobile Designer",
-    imgSrc: "./roberto.jpg", // Ruta correcta
+    imgSrc: "./roberto.jpg",
   },
   {
     name: "Marcelo Fazzito",
     title: "Mobile Designer",
-    imgSrc: "./marcelo.jpg", // Ruta correcta
+    imgSrc: "./marcelo.jpg",
   },
 ];
 
 const CardSlider = () => {
-  const [activeIndex, setActiveIndex] = useState(null);
-
-  const handleCardClick = (index) => {
-    setActiveIndex(index);
-  };
-
   return (
     <section className="card-slider">
+      {/* Título principal */}
+      <div className="card-slider__header">
+        <h1 className="card-slider__title">Nuestro Equipo</h1>
+        <p>
+          Detrás de nuestras soluciones hay personas comprometidas con cada uno
+          de los proyectos que llevamos adelante.
+        </p>
+      </div>
+
+      {/* Contenedor de tarjetas */}
       <div className="cards-container">
         {teamMembers.map((member, index) => (
-          <div
-            key={index}
-            className={`card ${activeIndex === index ? 'active' : ''}`}
-            onClick={() => handleCardClick(index)}
-          >
+          <div key={index} className="card">
             <div className="card__image">
               <img src={member.imgSrc} alt={member.name} />
             </div>
