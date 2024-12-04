af3133d182fcb805d66f8340710115927aa8f375
Francisco Godoy <sonrigodoy65@gmail.com>
2024-12-04 01:55:14 -0300

    header listo

--- src/App.js
+++ src/App.js
@@ -6,21 +6,20 @@ import CardSlider from './componentes/cardslider';
 import Logo from "./componentes/logo";
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
+import { Bars3Icon } from '@heroicons/react/24/outline';
+
 
 
 function App() {
   return (
     <div className="App flex flex-col min-h-screen"> {/* flex y min-h-screen para asegurar que el footer esté al final */}
       <header className="App-header">
+      <Bars3Icon className="h-6 w-6 text-gray-500" />
         <Header />
       </header>
       <main className="flex-grow">
         <Slider /> {/* Aquí está el Slider */}
-<<<<<<< HEAD
         <Logo/>
-=======
-        
->>>>>>> 89f128510a0c3eb0d2ab88b438da369c8988f190
         <CardSlider /> {/* Usa el CardSlider aquí */}
       </main>
       <footer className="App-footer">
--- src/componentes/header.jsx
+++ src/componentes/header.jsx
@@ -1,222 +1,173 @@
 import React, { useState } from 'react';
 import {
-  Dialog,
-  DialogPanel,
-  Disclosure,
-  DisclosureButton,
-  DisclosurePanel,
   Popover,
   PopoverButton,
   PopoverGroup,
   PopoverPanel,
 } from '@headlessui/react'; // Importa los componentes de Headless UI
 import {
-  ArrowPathIcon,
-  Bars3Icon,
-  ChartPieIcon,
-  CursorArrowRaysIcon,
-  FingerPrintIcon,
-  SquaresPlusIcon,
-  XMarkIcon,
-} from '@heroicons/react/24/outline'; // Importa los íconos de Heroicons (outline)
-import { ChevronDownIcon, PhoneIcon, PlayCircleIcon } from '@heroicons/react/20/solid'; // Importa los íconos de Heroicons (solid)
-import { ShoppingCartIcon, GlobeAltIcon } from '@heroicons/react/24/outline'; // Importamos los íconos adecuados
-import { Battery50Icon, CloudIcon, SunIcon, ShoppingBagIcon } from '@heroicons/react/24/outline'; // Importa los iconos adecuados
+  ChevronDownIcon,
+  PhoneIcon,
+  ShoppingCartIcon,
+  GlobeAltIcon,
+  Battery50Icon,
+  CloudIcon,
+  SunIcon,
+  ShoppingBagIcon,
+} from '@heroicons/react/24/outline'; // Importamos los íconos adecuados
+import { Bars3Icon } from '@heroicons/react/24/outline';
 
 const products = [
-  { name: 'Paneles Solares', href: '#', icon: SunIcon }, // Icono relacionado con paneles solares
-  { name: 'Hidroponia', href: '#', icon: ShoppingBagIcon }, // Icono de la naturaleza o alternativa
-  { name: 'Baterías', href: '#', icon: Battery50Icon }, // Icono de batería
-  { name: 'Energía Eólica', href: '#', icon: CloudIcon }, // Icono de energía eólica (puede ser un ícono relacionado con el viento)
+  { name: 'Paneles Solares', href: '#', icon: SunIcon },
+  { name: 'Hidroponia', href: '#', icon: ShoppingBagIcon },
+  { name: 'Baterías', href: '#', icon: Battery50Icon },
+  { name: 'Energía Eólica', href: '#', icon: CloudIcon },
 ];
 const callsToAction = [{ name: 'Ver Todo', href: '#', icon: PhoneIcon }];
 
 export default function Header() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false); // Estado para el menú móvil
-  const [popoverOpen, setPopoverOpen] = useState(false);
+  const [popoverOpen, setPopoverOpen] = useState(false); // Estado para el menú de productos
 
   return (
     <header className="bg-gray-900 fixed top-0 left-0 w-full z-50 shadow-lg">
       <nav
         aria-label="Global"
-        className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
+        className="mx-auto flex items-center justify-between p-6 lg:px-8"
       >
-        <div className="flex lg:flex-1">
+        {/* Logo a la izquierda, centrado un poco */}
+        <div className="flex flex-shrink-0 ml-4 lg:ml-0">
           <a href="#" className="-m-1.5 p-1.5">
             <span className="sr-only">Your Company</span>
-            <img
-              alt="Logo"
-              src="/logo512.png"
-              className="h-8 w-auto"
-            />
+            <img alt="Logo" src="/logo512.png" className="h-8 w-auto" />
           </a>
         </div>
 
-        {/* Botón para menú móvil */}
+        {/* Menú de productos en el centro */}
+        <div className="hidden lg:flex lg:gap-x-12 flex-1 justify-center">
+          <PopoverGroup className="flex items-center space-x-12">
+            <Popover className="relative">
+              <PopoverButton
+                onClick={() => setPopoverOpen(!popoverOpen)}
+                className="flex items-center gap-x-1 text-sm font-semibold text-white relative group"
+              >
+                Productos
+                <ChevronDownIcon
+                  className={`h-5 w-5 text-white transition-transform duration-200 ${popoverOpen ? 'rotate-180' : ''}`}
+                  aria-hidden="true"
+                />
+              </PopoverButton>
+
+              {popoverOpen && (
+                <PopoverPanel className="absolute -left-2 top-full z-10 mt-3 inline-block overflow-hidden rounded-4xl bg-white shadow-lg ring-1 ring-gray-900/5 min-w-[280px]">
+                  <div className="p-4">
+                    {products.map((item) => (
+                      <div
+                        key={item.name}
+                        className="group relative flex items-center gap-x-6 rounded-lg p-2 text-sm hover:bg-gray-50"
+                      >
+                        <div className="flex size-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
+                          <item.icon
+                            aria-hidden="true"
+                            className="h-6 w-6 text-black group-hover:text-indigo-600"
+                          />
+                        </div>
+                        <div className="flex-auto">
+                          <a
+                            href={item.href}
+                            className="block font-semibold text-gray-900 whitespace-nowrap"
+                          >
+                            {item.name}
+                            <span className="absolute inset-0" />
+                          </a>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                  <div className="flex justify-center bg-gray-50 p-4">
+                    {callsToAction.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className="flex items-center gap-x-2 px-6 py-3 text-sm font-semibold text-gray-900 bg-gray-200 rounded-md hover:bg-gray-300"
+                      >
+                        {item.name}
+                      </a>
+                    ))}
+                  </div>
+                </PopoverPanel>
+              )}
+            </Popover>
+
+            {/* Enlaces de navegación centralizados */}
+            {['Nosotros', 'Soluciones', 'Equipo', 'Contacto'].map((item) => (
+              <a
+                key={item}
+                href="#"
+                className="relative text-sm font-semibold text-white group"
+              >
+                {item}
+                <span className="absolute inset-x-0 bottom-0 h-[2px] scale-x-0 bg-indigo-600 transition-transform duration-300 group-hover:scale-x-100"></span>
+              </a>
+            ))}
+          </PopoverGroup>
+        </div>
+
+        {/* Íconos a la derecha, solo visibles en pantallas grandes */}
+        <div className="hidden lg:flex items-center space-x-4">
+          <ShoppingCartIcon className="h-6 w-6 text-white cursor-pointer hover:text-indigo-600 transition-all" />
+          <GlobeAltIcon className="h-6 w-6 text-white cursor-pointer hover:text-indigo-600 transition-all" />
+        </div>
+
+        {/* Botón para menú móvil a la derecha */}
         <div className="flex lg:hidden">
           <button
             type="button"
-            onClick={() => setMobileMenuOpen(!mobileMenuOpen)} // Cambia el estado del menú
+            onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
             className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-white"
           >
             <span className="sr-only">Open main menu</span>
             <Bars3Icon aria-hidden="true" className="h-6 w-6" />
           </button>
         </div>
-
-        {/* Menú en desktop */}
-        <PopoverGroup className="hidden lg:flex lg:gap-x-12">
-          <Popover
-            className="relative"
-            onMouseEnter={() => setPopoverOpen(true)}
-            onMouseLeave={() => setPopoverOpen(false)}
-          >
-            <PopoverButton className="flex items-center gap-x-1 text-sm font-semibold text-white relative group">
-              Productos
-              <span className="absolute inset-x-0 bottom-0 h-[2px] scale-x-0 bg-indigo-600 transition-transform duration-300 group-hover:scale-x-100"></span>
-            </PopoverButton>
-
-            {popoverOpen && (
-              <PopoverPanel
-                className="absolute -left-2 top-full z-10 mt-3 inline-block overflow-hidden rounded-4xl bg-white shadow-lg ring-1 ring-gray-900/5 min-w-[280px]"
-              >
-                <div className="p-4">
-                  {products.map((item) => (
-                    <div
-                      key={item.name}
-                      className="group relative flex items-center gap-x-6 rounded-lg p-2 text-sm hover:bg-gray-50"
-                    >
-                      <div className="flex size-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
-                        <item.icon
-                          aria-hidden="true"
-                          className="h-6 w-6 text-black group-hover:text-indigo-600"
-                        />
-                      </div>
-                      <div className="flex-auto">
-                        <a
-                          href={item.href}
-                          className="block font-semibold text-gray-900 whitespace-nowrap"
-                        >
-                          {item.name}
-                          <span className="absolute inset-0" />
-                        </a>
-                      </div>
-                    </div>
-                  ))}
-                </div>
-                <div className="flex justify-center bg-gray-50 p-4">
-                  {callsToAction.map((item) => (
-                    <a
-                      key={item.name}
-                      href={item.href}
-                      className="flex items-center gap-x-2 px-6 py-3 text-sm font-semibold text-gray-900 bg-gray-200 rounded-md hover:bg-gray-300"
-                    >
-                      {item.name} {/* Eliminar el ícono */}
-                    </a>
-                  ))}
-                </div>
-              </PopoverPanel>
-            )}
-          </Popover>
-
-          {['Nosotros', 'Soluciones', 'Equipo', 'Contacto'].map((item) => (
-            <a
-              key={item}
-              href="#"
-              className="relative text-sm font-semibold text-white group"
-            >
-              {item}
-              <span className="absolute inset-x-0 bottom-0 h-[2px] scale-x-0 bg-indigo-600 transition-transform duration-300 group-hover:scale-x-100"></span>
-            </a>
-          ))}
-        </PopoverGroup>
-
-        {/* Iconos de carrito y lenguaje */}
-        <div className="hidden lg:flex lg:flex-1 lg:justify-end space-x-6">
-          <a href="#" className="relative text-white group mb-2 hover:text-indigo-300">
-            <ShoppingCartIcon aria-hidden="true" className="h-8 w-8 transition duration-200 ease-in-out" />
-            <span className="absolute inset-x-0 bottom-[-4px] h-[2px] scale-x-0 bg-indigo-600 transition-transform duration-300"></span>
-          </a>
-          <a href="#" className="relative text-white group mb-2 hover:text-indigo-300">
-            <GlobeAltIcon aria-hidden="true" className="h-8 w-8 transition duration-200 ease-in-out" />
-            <span className="absolute inset-x-0 bottom-[-4px] h-[2px] scale-x-0 bg-indigo-600 transition-transform duration-300"></span>
-          </a>
-        </div>
       </nav>
-     {/* Menú móvil */}
-{mobileMenuOpen && (
-  <div className="fixed inset-0 bg-black bg-opacity-50 backdrop-blur-sm z-50">
-    <div 
-      className={`fixed inset-y-0 right-0 w-64 bg-gray-900 text-white p-6 transform ${mobileMenuOpen ? 'translate-x-0' : 'translate-x-full'} transition-transform duration-3000`}
-    >
-      {/* Logo alineado a la izquierda y Botón de Cierre */}
-      <div className="flex justify-between mb-6">
-        <img
-          src="/logo512.png"
-          alt="Logo"
-          className="h-8 w-auto"
-        />
-        <button
-          onClick={() => setMobileMenuOpen(false)}
-          className="text-white"
-        >
-          <XMarkIcon className="h-6 w-6" />
-        </button>
-      </div>
 
-      {/* Línea debajo del logo */}
-      <div className="border-b-2 border-white mb-4"></div>
+      {/* Menú en mobile con fondo borroso */}
+      {mobileMenuOpen && (
+        <div className="lg:hidden fixed top-0 right-0 w-full h-full bg-black bg-opacity-50 backdrop-blur-sm transition-all duration-500 ease-in-out">
+          <div className="bg-gray-800 text-white p-4 w-64 h-full fixed right-0 top-0 transform translate-x-0 transition-transform duration-500 ease-in-out">
+            {/* Cerrar el menú con una X más pequeña */}
+            <button
+              onClick={() => setMobileMenuOpen(false)}
+              className="absolute top-4 right-4 text-white text-xl"
+            >
+              X
+            </button>
+            {/* Logo en el menú móvil */}
+            <div className="flex justify-between mb-8">
+              <img alt="Logo" src="/logo512.png" className="h-8 w-auto" />
+            </div>
 
-      {/* Enlaces del menú */}
-      <div className="flex flex-col mt-4 space-y-4">
-        {['Nosotros', 'Soluciones', 'Equipo', 'Contacto'].map((item) => (
-          <a
-            key={item}
-            href="#"
-            className="text-white text-xl mb-2 relative hover:text-indigo-500 transition-all duration-200"
-          >
-            {item}
-          </a>
-        ))}
-
-        {/* Menú de Productos con línea debajo */}
-        <Popover className="relative">
-          <PopoverButton className="text-white text-xl mb-4 relative hover:text-indigo-500 transition-all duration-200">
-            Productos
-          </PopoverButton>
-          {popoverOpen && (
-            <PopoverPanel className="absolute top-full bg-white text-black p-4">
-              {products.map((item) => (
-                <a key={item.name} href={item.href} className="block mb-4">
-                  {item.name}
+            {/* Enlaces del menú móvil con hover */}
+            <div className="flex flex-col space-y-4">
+              {['Nosotros', 'Soluciones', 'Equipo', 'Contacto'].map((item) => (
+                <a
+                  key={item}
+                  href="#"
+                  className="text-sm font-semibold py-3 px-4 hover:bg-gray-700 rounded-md transition-all"
+                >
+                  {item}
                 </a>
               ))}
-            </PopoverPanel>
-          )}
-        </Popover>
-        <div className="border-b-2 border-white mb-4"></div>
-      </div>
-
-      {/* Íconos de Carrito y Cambio de Idioma al lado */}
-      <div className="mt-6 flex justify-center space-x-6">
-        <a
-          href="#"
-          className="text-white hover:text-indigo-500 transition-all duration-200 flex items-center"
-        >
-          <ShoppingCartIcon className="h-6 w-6" />
-        </a>
-        <a
-          href="#"
-          className="text-white hover:text-indigo-500 transition-all duration-200 flex items-center"
-        >
-          <GlobeAltIcon className="h-6 w-6" />
-        </a>
-      </div>
-    </div>
-  </div>
-)}
-
-
-</header>
+              {/* Íconos del menú móvil, centrados */}
+              <div className="flex justify-center items-center space-x-6 mt-8">
+                <ShoppingCartIcon className="h-6 w-6 text-white cursor-pointer hover:text-indigo-600 transition-all" />
+                <GlobeAltIcon className="h-6 w-6 text-white cursor-pointer hover:text-indigo-600 transition-all" />
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </header>
   );
 }
