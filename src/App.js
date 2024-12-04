ea6659386a6e9170758e2f4e14eae1963d3d6ffb
Francisco Godoy <sonrigodoy65@gmail.com>
2024-12-04 01:00:02 -0300

    json

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Logo from "./componentes/logo";
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 
+
 function App() {
   return (
     <div className="App flex flex-col min-h-screen"> {/* flex y min-h-screen para asegurar que el footer esté al final */}
@@ -15,7 +16,11 @@ function App() {
       </header>
       <main className="flex-grow">
         <Slider /> {/* Aquí está el Slider */}
+<<<<<<< HEAD
         <Logo/>
+=======
+        
+>>>>>>> 89f128510a0c3eb0d2ab88b438da369c8988f190
         <CardSlider /> {/* Usa el CardSlider aquí */}
       </main>
       <footer className="App-footer">
